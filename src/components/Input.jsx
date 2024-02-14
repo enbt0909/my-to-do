c0ae8fa065dd260ec3edd651ceb24fa187c3e9ad
enbt0909 <eugenbeirit22@gmail.com>
2024-02-14 13:42:32 +0100

    input

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -3,6 +3,7 @@ import './Input.css';
 
 function Input(props) {
   const [text, setText] = useState('');
+  const [message, setMessage] = useState('');
 
   const handleChange = (event) => {
     setText(event.target.value);
@@ -11,6 +12,12 @@ function Input(props) {
   const handleSave = () => {
     props.onSave(text);
     setText('');
+    setMessage('GESPEICHERT');
+
+    // Meldung nach 2 Sekunden entfernen
+    setTimeout(() => {
+      setMessage('');
+    }, 1000);
   };
 
   return (
@@ -18,6 +25,7 @@ function Input(props) {
       <input type="text" value={text} onChange={handleChange} className="input-field" placeholder='hier eingeben und' />
       <p className='char__counter' style={{ fontSize: '16px' }}>{text.length}</p>
       <button onClick={handleSave} className="save-button">Speichern</button>
+      {message && <p>{message}</p>}
     </div>
   );
 }
